0dd387eef4bb1797bf3642b8275a467c1b140bf9
himanshu-tiwari <htiwarih@gmail.com>
2019-12-30 19:06:57 +0530

    Created basic setup

---
+++ src/components/PictureGrid/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.scss';
+
+const PictureGrid = props => {
+    return <div className="picture-grid">Grid</div>;
+};
+
+export default PictureGrid;
---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.scss';
+
+const SearchBar = props => {
+    return <div className="search-bar">search-bar</div>;
+};
+
+export default SearchBar;
