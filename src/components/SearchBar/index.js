aca236d0082fbc1fb82bc1a1babfb45b2f666f53
himanshu-tiwari <htiwarih@gmail.com>
2019-12-31 11:41:58 +0530

    Setup the axios queries

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './index.scss';
+import { PictureContext } from '../../contexts/PictureContext';
+import { searchPictures, loadMorePictures } from '../../queries/pictures';
 
 const SearchBar = props => {
+    const { loading, dispatch, text, photos } = useContext(PictureContext);
+    
+    useEffect(() => {
+        // dispatch({ type: 'BEGIN_SEARCH' });
+        // searchPictures(dispatch, { text: "car" });
+        
+        dispatch({ type: 'BEGIN_LOAD_MORE' });
+        loadMorePictures(dispatch, { text, page: photos.page+1 });
+    }, [text]);
+
     return <div className="search-bar">search-bar</div>;
 };
 
---
+++ src/queries/pictures.js
@@ -0,0 +1,85 @@
+import Axios from 'axios';
+import { key } from '../config/flickr';
+
+export const searchPictures = async (dispatch, input) => {
+    try {
+        const response = await Axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${
+                key
+            }&text=${
+                ""
+            }&media=photos&per_page=10&page=${
+                1
+            }&format=json&nojsoncallback=1`);
+            
+        if (response.data && typeof(response.data) === "object") {
+            dispatch({
+                type: "SEARCH_PICTURES_SUCCESS",
+                payload: {
+                    ...response.data,
+                    loading: false,
+                    text: input.text
+                }
+            });
+        } else {
+            dispatch({
+                type: "SEARCH_PICTURES_FAILURE",
+                payload: {
+                    stat: "fail",
+                    message: "Unexpected error!",
+                    loading: false,
+                    text: input.text
+                }
+            });
+        }
+    } catch (error) {
+        dispatch({ 
+            type: "SEARCH_PICTURES_FAILURE",
+            payload: {
+                stat: "fail",
+                message: error.message,
+                loading: false,
+                text: input.text
+            }
+        });
+    }
+}
+
+export const loadMorePictures = async (dispatch, input) => {
+    try {
+        const response = await Axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${
+                key
+            }&text=${
+                input.text
+            }&media=photos&per_page=10&page=${
+                input.page
+            }&format=json&nojsoncallback=1`);
+        
+        if (response.data && typeof(response.data) === "object") {
+            dispatch({
+                type: "LOAD_MORE_SUCCESS",
+                payload: {
+                    ...response.data,
+                    fetchingMore: false
+                }
+            });
+        } else {
+            dispatch({
+                type: "LOAD_MORE_FAILURE",
+                payload: {
+                    stat: "fail",
+                    message: "Unexpected error!",
+                    fetchingMore: false
+                }
+            });
+        }
+    } catch (error) {
+        dispatch({
+            type: "LOAD_MORE_FAILURE",
+            payload: {
+                stat: "fail",
+                message: error.message,
+                fetchingMore: false
+            }
+        });
+    };
+};
