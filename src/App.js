5875facd5ac54f45e01f59c69c496124b269ae1e
himanshu-tiwari <htiwarih@gmail.com>
2019-12-31 11:43:12 +0530

    Changed the context provider setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.scss';
 import SearchBar from './components/SearchBar';
 import PictureGrid from './components/PictureGrid';
+import 'antd/dist/antd.css';
 
 const App = props => {
 	return <div className="App">
--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import PictureContextProvider from './contexts/PictureContext';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <PictureContextProvider>
+        <App />
+    </PictureContextProvider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
